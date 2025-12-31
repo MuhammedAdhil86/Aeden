59dca16a738a3b0281f16daea2f9c8da8a0f7d00
muhammed <adhilnizar86@gmail.com>
2025-12-31 11:41:32 +0530

    Describe what you changed, e.g.,

--- src/app/admin/benchMarking/details/components/chartsecsion.jsx
+++ src/app/admin/benchMarking/details/components/chartsecsion.jsx
@@ -2,7 +2,7 @@
 
 import React from "react";
 import BenchmarkPriceChart from "@/components/ui/monthlyRevenueChart";
-import SummaryCard from "./Summarycard";
+import SummaryCard from "./summarycard";
 
 /**
  * ChartSection Component
